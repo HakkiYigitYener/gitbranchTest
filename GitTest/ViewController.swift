e981a7d1ec473c53b02b77a59c15f6c68d22af61
Hakki <hyener@invio.com>
2020-04-15 01:33:50 +0300

    # This is a combination of 4 commits. # This is the 1st commit message:
    First Commit.
    
    # This is the commit message #2:
    
    Pod eklendi.
    
    # This is the commit message #3:
    
    IQ Keyboard eklendi.
    
    # This is the commit message #4:
    
    001

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Hakkı Yiğit Yener on 15.04.2020.
+//  Copyright © 2020 HakkiYigitYener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func feature001() -> Void {
+        
+    }
+
+}
+
