21939d583cd10cb36ea0837eeac576dff4e1ac6f
Hakki <hyener@invio.com>
2020-04-15 01:33:50 +0300

    parent 7df89851ffe84303f9f4fd2ffdf0746d82bc29be author Hakki <hyener@invio.com> 1586903630 +0300 committer Hakki <hyener@invio.com> 1586905974 +0300
    First Commit.
    
    Pod eklendi.
    
    IQ Keyboard eklendi.
    
    001
    
    003
    
    004
    
    005

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Hakkı Yiğit Yener on 15.04.2020.
+//  Copyright © 2020 HakkiYigitYener. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func feature001() -> Void {
+        
+    }
+    
+    func feature002() -> Void {
+        
+    }
+    
+    func feature003() -> Void {
+        
+    }
+    
+    func feature004() -> Void {
+        
+    }
+
+}
+
